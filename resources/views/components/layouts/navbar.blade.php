0c61d7bd53c43d012a18666ba117c8ccce155b95
JimyCalvo <jbcm1152@gmail.com>
2023-01-05 20:12:18 -0500

    listo2

--- resources/views/components/layouts/navbar.blade.php
+++ resources/views/components/layouts/navbar.blade.php
@@ -6,8 +6,8 @@
 		
 			<div class="nav_list">
 				<a href="{{route('home')}}"  class="nav_link"> <i class='bx bx-home nav_icon'></i> <span class="nav_name">Inicio</span> </a>
-				<a href="{{route('restaurantes.index')}}" class="nav_link"> <i class='bx bx-receipt nav_icon'></i> <span class="nav_name">Locales</span> </a>
-				<a href="{{route('platos.index')}}" class="nav_link"> <i class='bx bx-search nav_icon'></i> <span class="nav_name">Platos</span> </a>
+				<a href="{{route('restaurantes.index')}}" class="nav_link"> <i class='bx bx-search nav_icon'></i> <span class="nav_name">Locales</span> </a>
+				<a href="{{route('platos.index')}}" class="nav_link"> <i class=' bx bx-receipt nav_icon'></i> <span class="nav_name">Platos</span> </a>
 				<a href="{{route('categorias.index')}}" class="nav_link"> <i class='bx bx-basket nav_icon'></i> <span class="nav_name">Categorias</span> </a>
 				<a href="{{route('places')}}" class="nav_link"> <i class='bx bx-map nav_icon'></i> <span class="nav_name">Ubicaciones</span> </a>
 				<a href="{{route('about')}}" class="nav_link"> <i class='bx bx-info-circle nav_icon'></i> <span class="nav_name">Acerca de Nosotros</span> </a>
--- resources/views/restaurantes/index.blade.php
+++ resources/views/restaurantes/index.blade.php
@@ -1,10 +1,8 @@
 
 
 
-
 <x-layouts.app title="Inicio">
    
-
     <h1>Locales Busqueda e información</h1>
 
     <a href="{{ route('restaurantes.create') }}">Crear nuevo Local</a>
@@ -21,27 +19,35 @@
 
             <br>
             <br>
+
+
         @foreach ($restaurantes as $restaurante)
-        <div style="display:flex; aling-items: baseline" class="contanier me-5 ms-5 pe-5 ps-5">
-            <img src="{{asset($restaurante->fotoDirec)}}" alt="    {{$restaurante->nombre_restaurante}}"   width="120 px">
-            <p>
-                <a href="{{ route('restaurantes.show', $restaurante) }}">
-                    {{ $restaurante->nombre_restaurante }}  
-                </a>
-            </p> &nbsp;
-        
-            &nbsp;
-
-            <a class="btn btn-link" href="{{route('restaurantes.edit', $restaurante)}}">Editar</a>
-
-            &nbsp;
-
-            <form action="{{route('restaurantes.destroy', $restaurante)}}" method="POST">
-                @csrf
-            @method('DELETE')
-
-            <button type="submit" class="btn btn btn-danger" >Eliminar</button>
-            </form>
+        <div class="container row justify-content-md-center">
+            <div class="card" style="width: 50%;">
+                <img src="{{asset($restaurante->fotoDirec)}}" class="card-img-top" alt="    {{$restaurante->nombre_restaurante}}">
+                <div class="card-body">
+                  <h5 class="card-title">
+                    <a href="{{ route('restaurantes.show', $restaurante) }}">
+                        {{ $restaurante->nombre_restaurante }}  
+                    </a>
+                </h5> 
+                  <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint facere possimus ipsum totam perferendis recusandae, corrupti labore nostrum vel doloremque cum aut ut quam exercitationem saepe asperiores repudiandae nisi iure.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Dirección:      {{$restaurante->direccion_local}}</li>
+                    <li class="list-group-item" >Dueño/Gerente: {{$restaurante->gerente_local}}</li>
+                    <li class="list-group-item">Tipo de local:  {{$restaurante->tipo_local}}</li>
+                    <li class="list-group-item">Correo:         {{$restaurante->correo_local}}</li>
+                </ul>
+                <div class="container p-2 text-center">
+                    <a name="editas" id="editar" class="btn btn-primary" href="{{route('restaurantes.edit', $restaurante)}}" role="button" style="width: 130px">Editar</a>
+                    <form action="{{route('restaurantes.destroy', $restaurante)}}" method="POST">
+                        @csrf
+                         @method('DELETE')
+                        <button type="submit" class="btn btn btn-danger mt-3" style="width: 130px" >Eliminar</button>
+                    </form>
+                </div>
+            </div>
         </div>
         @endforeach
     
